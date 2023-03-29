215f4f072c84bd491fe6fbb891c329db8d1370ab
isahua16 <isaelhuard@gmail.com>
2023-03-28 21:18:07 -0600

    Added objects of pokemons and the html and css

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let pokemons = [
+    {
+        name:`Charmander`,
+        image_url: `/images/charmander.png`,
+        description: `Charmander is a fire-type Pokemon that resembles a small orange lizard with a flame at the end of its tail. It has large, blue eyes and a pointed snout. Charmander's flame burns brighter as it becomes stronger, and it is said that when the flame goes out, Charmander will die. Charmander is known for its fierce determination and loyalty to its trainer, making it a popular choice among Pokemon trainers.`
+    },
+    {
+        name: `Squirtle`,
+        image_url: `/images/squirtle.png`,
+        description: `Squirtle is a water-type Pokemon that resembles a small blue turtle with a shell on its back. Its eyes are large and brown, and it has long, pointed ears. Squirtle is known for its powerful water attacks, which it can shoot from its mouth or manipulate with its tail. It is also able to retract its limbs and head into its shell for protection. Squirtle is a loyal and friendly Pokemon, making it a popular choice for trainers who value cooperation and teamwork.`
+    },
+    {
+        name: `Bulbasaur`,
+        image_url: `/images/bulbasaur.png`,
+        description: `Bulbasaur is a grass/poison-type Pokemon that looks like a small, squat dinosaur with a large, green bulb on its back. The bulb contains nutrients that Bulbasaur needs to survive, and it can also release powerful attacks. Bulbasaur's eyes are a bright red color, and it has pointed ears and sharp teeth. This Pokemon is known for its calm and patient nature, as well as its ability to quickly adapt to different environments.`
+    }
+];
+
